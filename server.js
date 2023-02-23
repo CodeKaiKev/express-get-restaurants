39aabed8a21b7e7cd281cc0b9aaecfbd961b82ec
Codekaikev <codekaikev@gmail.com>
2023-02-23 00:33:33 +0000

    Part 3

--- server.js
+++ server.js
@@ -12,7 +12,36 @@ app.get("/restaurants", async (request, response)  => {
 })
 app.get("/restaurants/:id", async (request, response)  => {
     const restaurant = await Restaurant.findByPk(request.params.id);
-    response.json(restaurant);
+    response.json({restaurant});
+})
+
+app.use(express.json());
+
+//Post for adding a new restaurant
+app.post("/restaurants", async ( request, response) => {
+    
+    const newRestaurant = await Restaurant.create(request.body);
+    response.json(Restaurant);
+})
+//Put for updating a new restaurant
+app.put("/restaurants/:id", async (request, response) => {
+    const newRestaurant = request.body;
+    await Restaurant.update(
+        {name: newRestaurant.name, location: newRestaurant.location, cuisine: newRestaurant.cuisine},
+        {where: {id: request.params.id}}
+        )
+
+    //const res = await Restaurant.findAll();
+    response.json(Restaurant);
+})
+//Deleting a restaurant based on ID
+app.delete("/restaurants/:id", async (request, response) => {
+    const deletedRestaurant = await Restaurant.destroy({
+        where: {
+            id: request.params.id
+        }
+    })
+    response.json(Restaurant);
 })
 
 // app.listen(port, () => {
@@ -21,5 +50,5 @@ app.get("/restaurants/:id", async (request, response)  => {
 // })
 app.listen(port, () => {
     sequelize.sync();
-    console.log("Your server is listening on port http://localhost:3000/restaurants/2");
+    console.log("Your server is listening on port http://localhost:3000/restaurants");
 })
