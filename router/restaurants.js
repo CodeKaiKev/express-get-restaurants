d117543d377cf5c96d5b34abc152073964015aaa
Codekaikev <codekaikev@gmail.com>
2023-02-24 20:33:33 +0000

    Part 5

--- router/restaurants.js
+++ router/restaurants.js
@@ -3,7 +3,7 @@ const express = require("express");
 const {Restaurant} = require("../models/index");
 const {sequelize} = require("../db");
 const router = express.Router();
-//const {check, validationResult} = require("express-validator");
+const {check, validationResult} = require("express-validator");
 
 //TODO: Create your GET Request Route Below: 
 router.get("/", async (request, response)  => {
@@ -19,13 +19,19 @@ router.get("/:id", async (request, response)  => {
     response.json({restaurant});
 })
 
-router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
 
 //Post for adding a new restaurant
-router.post("/", async ( request, response) => {
+router.post("/", [check("name", "Not Empty or white space").not().isEmpty().trim(), check("location", "Not Empty or white space").not().isEmpty().trim(), check("cuisine", "Not Empty or white space").not().isEmpty().trim(), check("name", "Has to be between 10 and 30").isLength({min: 10, max: 30})], async ( request, response) => {
     
-    const newRestaurant = await Restaurant.create(request.body);
-    response.json(Restaurant);
+    const errors = validationResult(request);
+    if(!errors.isEmpty()) {
+        response.json({error: errors.array()});
+    }else {
+        const newRestaurant = await Restaurant.create(request.body);
+        response.json(Restaurant);
+    }
+ 
 })
 //Put for updating a new restaurant
 router.put("/:id", async (request, response) => {
